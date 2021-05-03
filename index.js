1ff170ce9bcbdd942a94ea129ad2318f28032374
Carly <carlycarswell@gmail.com>
2021-05-03 17:44:52 +0100

    Add collision, game states, score

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const canvas = document.querySelector("#canvas");
 
 const c = canvas.getContext("2d");
 
-canvas.height = innerHeight - 5;
+canvas.height = innerHeight;
 
-canvas.width = innerWidth - 5;
+canvas.width = innerWidth;
+
+addEventListener("resize", () => {
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+});
+
+const container = document.querySelector(".container");
+
+const scoreText = document.querySelector(".score");
 
 // Objects
 
@@ -15,8 +24,8 @@ class Bird {
     this.y = y;
     this.velocity = velocity;
     this.colour = "yellow";
-    this.width = 60;
-    this.height = 60;
+    this.width = 45;
+    this.height = 45;
   }
 
   draw() {
@@ -27,18 +36,17 @@ class Bird {
   update() {
     this.draw();
     this.y = this.y + this.velocity.y;
-    
   }
 }
 
 class Pipe {
-  constructor(height, velocity, orientation) {
+  constructor(height, velocity, orientationBottom) {
     this.colour = "green";
     this.width = 100;
     this.height = height;
-    this.orientation = orientation;
+    this.orientationBottom = orientationBottom;
     this.x = canvas.width + this.width;
-    this.y = this.orientation ? canvas.height - this.height : 0;
+    this.y = this.orientationBottom ? canvas.height - this.height : 0;
     this.velocity = velocity;
   }
 
@@ -50,85 +58,120 @@ class Pipe {
   update() {
     this.draw();
     this.x = this.x - this.velocity;
-
-
-    // const playerTop = player.y;
-    // const playerBottom = player.y + player.height;
-    // const playerLeft = player.x;
-    // const playerRight = player.x + player.width;
-
-    // const pipeLeft = this.x;
-    // const pipeRight = this.x + this.width;
-    // const pipeTop = this.y;
-
-
-    // if(playerTop )
-
   }
 }
 
-const player = new Bird(50, canvas.height / 2 - 100, { x: 0, y: 5 });
+let player;
 
-const pipes = [];
+let pipes;
+
+let score;
 
 let animationId;
 
+let timer;
+
+let interval;
+
+let gameOn = false;
+
 // Functions + event listeners
 
-addEventListener('click', () => {
-    player.velocity.y = -6;
-
-
-    console.log(player.velocity.y)
-    setTimeout(() => {
-        player.velocity.y = 5;
-    }, 600)
-})
-
 function spawnPipes() {
-  let orientation = true;
+  let orientationBottom = true;
 
-  setInterval(() => {
-    const pipe = new Pipe(Math.random() * (500 - 300) + 300, 10, orientation);
+  interval = setInterval(() => {
+    const pipe = new Pipe(
+      Math.random() * (500 - 300) + 300,
+      10,
+      orientationBottom
+    );
 
     pipes.push(pipe);
 
-    orientation = !orientation;
+    // swap top/bottom
+    orientationBottom = !orientationBottom;
   }, 1000);
 }
 
 function animate() {
-    animationId = requestAnimationFrame(animate);
+  animationId = requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
 
   player.update();
 
+  // collision offscreen
   if (player.y + player.height > canvas.height) {
-    console.log("offscreen");
-    cancelAnimationFrame(animationId);
+    gameOver();
   }
 
   pipes.forEach((pipe, index) => {
     pipe.update();
-    if (pipe.x + pipe.width < 0) {
+
+    // collision with pipes
+    if (
+      player.x < pipe.x + pipe.width &&
+      player.x + player.width > pipe.x &&
+      player.y < pipe.y + pipe.height &&
+      player.y + player.height > pipe.y
+    ) {
+      gameOver();
+    }
+
+    // remove offscreen pipes + increase score
+    if (pipe.x + pipe.width < -30) {
+
+      score += 10;
+
       setTimeout(() => {
         pipes.splice(index, 1);
       }, 0);
     }
   });
-
 }
-// Init game
 
-animate();
-spawnPipes();
+function gameOver(){
+  gameOn = false;
+  
+  clearInterval(interval);
+  cancelAnimationFrame(animationId);
 
+  container.style.display = "flex";
+  scoreText.innerHTML = `SCORE: ${score}`
+  scoreText.style.display = "block";
+}
+
+function init() {
+  container.style.display = "none";
+
+  player = new Bird(50, canvas.height / 2 - 100, { x: 0, y: 5 });
+  pipes = [];
+  score = 0;
+
+  gameOn = true;
+
+  animate();
+  spawnPipes();
+}
+
+// space bar starts game if not running, else moves bird
+
+addEventListener("keydown", (event) => {
+  if (event.key === " " && gameOn === true) {
+    // allow repeat presses to cancel gravity
+    clearTimeout(timer);
+
+    player.velocity.y = -4;
+
+    timer = setTimeout(() => {
+      player.velocity.y = 7;
+    }, 600);
+  } else if (event.key === " " && gameOn === false) {
+    init();
+  }
+});
 
 // TODO
-// player falling
-// player click to move
-// collision
-// score
-// start + gameover menu
+// pipe height as percentage of window height
 // sfx?
-// better graphics
+// better graphics.....
